1ee1d74c744720b6ff7bad6e27178c2ab420787a
elKryptos <154229030+elKryptos@users.noreply.github.com>
2024-09-16 20:49:48 +0200

    added jwt, bouncycastle, spring security dependencies, added private.pem and public.pem e key.properties file

--- PetFinder-Backend/src/main/java/hans/startup/petfinderbackend/PetFinderBackendApplication.java
+++ PetFinder-Backend/src/main/java/hans/startup/petfinderbackend/PetFinderBackendApplication.java
@@ -3,6 +3,7 @@ package hans.startup.petfinderbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PetFinderBackendApplication {
 
---
+++ PetFinder-Backend/src/main/java/hans/startup/petfinderbackend/utils/JwtToken.java
@@ -0,0 +1,5 @@
+package hans.startup.petfinderbackend.utils;
+
+public class JwtToken {
+    
+}
