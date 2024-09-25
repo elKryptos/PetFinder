da73e3b368f9936ce2ea1df1ebb72f56aeff69bd
elKryptos <154229030+elKryptos@users.noreply.github.com>
2024-09-25 19:09:57 +0200

    added MediaController & UploadMediaResponse

---
+++ PetFinder-Backend/src/main/java/hans/startup/petfinderbackend/responses/UploadMediaResponse.java
@@ -0,0 +1,10 @@
+package hans.startup.petfinderbackend.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class UploadMediaResponse {
+    private String path;
+}
