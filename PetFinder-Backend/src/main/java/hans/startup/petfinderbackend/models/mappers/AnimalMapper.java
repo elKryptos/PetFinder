170012edb1bb449d1d66c120a3bb3da892ee278e
elKryptos <you@example.com>
2024-10-31 17:09:48 +0100

    added mapstruct package

---
+++ PetFinder-Backend/src/main/java/hans/startup/petfinderbackend/models/mappers/AnimalMapper.java
@@ -0,0 +1,15 @@
+package hans.startup.petfinderbackend.models.mappers;
+
+import hans.startup.petfinderbackend.models.dtos.AnimalDto;
+import hans.startup.petfinderbackend.models.entities.Animal;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface AnimalMapper {
+    AnimalDto toDto(Animal animal);
+    Animal toEntity(AnimalDto animalDto);
+    List<AnimalDto> toDtoList(List<Animal> animalList);
+    List<Animal> toEntityList(List<AnimalDto> animalDtoList);
+}
---
+++ PetFinder-Backend/src/main/java/hans/startup/petfinderbackend/models/mappers/UserMapper.java
@@ -0,0 +1,15 @@
+package hans.startup.petfinderbackend.models.mappers;
+
+import hans.startup.petfinderbackend.models.dtos.UserDto;
+import hans.startup.petfinderbackend.models.entities.User;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    UserDto toDto(User user);
+    User toEntity(UserDto userDto);
+    List<UserDto> toDtoList(List<User> userList);
+    List<User> toEntityList(List<UserDto> userDtoList);
+}
